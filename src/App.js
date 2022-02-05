cd3b71a313572fd325a764a13c3a9165fd456c2d
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-02-05 10:23:16 +0100

    carousel card (3d)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.scss';
+import CarouselCard from './components/carousel/carousel-card/CarouselCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CarouselCard />
     </div>
   );
 }
---
+++ src/components/carousel/carousel-card/CarouselCard.js
@@ -0,0 +1,27 @@
+import "./style.scss";
+import TestImage from "../test-image.png";
+
+const CarouselCard = () => {
+  return (
+    <div className="carousel__card">
+      <div className="carousel__body">
+        <div className="carousel__image">
+          <img src={TestImage} alt="author__img" />
+        </div>
+        <div className="carousel__content">
+          <p className="quote">
+            Start your project quickly with our fast matching system. Get
+            matched with talents that fit you perfectly within a short
+            time.Start your project quickly with our fast matching system.
+          </p>
+          <div className="profile">
+            <p className="profile-head">Timothy Agrena</p>
+            <p className="profile-title">C.E.O Leil&bey</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarouselCard;
